a8f222189114b57ad636a94e560d81398469d8fd
vijendra1125 <vijendrasingh003@gmail.com>
2020-07-18 16:19:51 +0530

    week3 pa 5 done

--- algorithmic_toolbox/programming_assignments/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
+++ algorithmic_toolbox/programming_assignments/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
@@ -7,13 +7,61 @@
 using std::string;
 using std::vector;
 
+vector<string> get_max_candidates(vector<string> v, int digit_position)
+{
+  vector<string> max_candidates;
+  char max = 0;
+  for (size_t i = 0; i < v.size(); ++i)
+  {
+    char digit = v[i][digit_position % v[i].length()];
+    if (digit > max)
+    {
+      max_candidates.clear();
+      max = digit;
+      max_candidates.push_back(v[i]);
+    }
+    else if (digit == max)
+    {
+      max_candidates.push_back(v[i]);
+    }
+  }
+  return max_candidates;
+}
+
 string largest_number(vector<string> a)
 {
-  //write your code here
-  std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++)
+  vector<string> remaining_numbers = a;
+  vector<string> arranged_numbers;
+  while (remaining_numbers.size() > 1)
   {
-    ret << a[i];
+    vector<string> candidates = remaining_numbers;
+    for (int i = 0; i < 4; ++i)
+    {
+      vector<string> temp = get_max_candidates(candidates, i);
+      candidates.clear();
+      candidates = temp;
+    }
+
+    for (size_t i = 0; i < candidates.size(); ++i)
+    {
+      arranged_numbers.push_back(candidates[i]);
+    }
+    vector<string>::iterator ip;
+    ip = std::unique(candidates.begin(), candidates.end());
+    candidates.resize(std::distance(candidates.begin(), ip));
+    for (int i = 0; i < candidates.size(); ++i)
+    {
+      remaining_numbers.erase(remove(remaining_numbers.begin(),
+                                     remaining_numbers.end(), candidates[i]),
+                              remaining_numbers.end());
+    }
+  }
+  arranged_numbers.push_back(remaining_numbers[0]);
+
+  std::stringstream ret;
+  for (size_t i = 0; i < arranged_numbers.size(); i++)
+  {
+    ret << arranged_numbers[i];
   }
   string result;
   ret >> result;
@@ -29,5 +77,5 @@ int main()
   {
     std::cin >> a[i];
   }
-  std::cout << largest_number(a);
+  std::cout << largest_number(a) << "\n";
 }
