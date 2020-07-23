4b7ac85db1c24603bc88b54780c79eb96e8b57e2
vijendra1125 <vijendrasingh003@gmail.com>
2020-07-23 11:58:10 +0530

    week 4 pa 4,5

--- algorithmic_toolbox/programming_assignment_solutions/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
+++ algorithmic_toolbox/programming_assignment_solutions/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
@@ -3,57 +3,57 @@
 
 using std::vector;
 
-int merge(vector<int> &e, vector<int> &c, vector<int> &d, int idx)
+long long merge(vector<int> &main_vec, vector<int> &left_vec, vector<int> &right_vec, int r)
 {
-  int inv = 0;
-  while ((c.size() > 0) && (d.size() > 0))
+  long long inv = 0;
+  while ((left_vec.size() > 0) && (right_vec.size() > 0))
   {
-    if (c.back() <= d.back())
+    if (left_vec.back() <= right_vec.back())
     {
-      e[idx] = c.back();
-      idx--;
-      c.erase(c.end() - 1);
+      main_vec[r] = left_vec.back();
+      r--;
+      left_vec.erase(left_vec.end() - 1);
     }
     else
     {
-      inv += c.size();
-      e[idx] = d.back();
-      idx--;
-      d.erase(d.end() - 1);
+      inv += left_vec.size();
+      main_vec[r] = right_vec.back();
+      r--;
+      right_vec.erase(right_vec.end() - 1);
     }
+  }
 
-    if (c.size() != 0)
+  if (left_vec.size() != 0)
+  {
+    for (int i = left_vec.size() - 1; i >= 0; --i)
     {
-      for (int i = 0; i < c.size(); ++i)
-      {
-        e[i] = c[i];
-      }
+      main_vec[r] = left_vec[i];
+      r--;
     }
-    if (d.size() != 0)
+  }
+  if (right_vec.size() != 0)
+  {
+    for (int i = right_vec.size() - 1; i >= 0; --i)
     {
-      for (int i = 0; i < d.size(); ++i)
-      {
-        e[i] = d[i];
-      }
+      main_vec[r] = right_vec[i];
+      r--;
     }
   }
   return inv;
 }
 
-long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right)
+long long get_number_of_inversions(vector<int> &a, size_t left, size_t right)
 {
   long long number_of_inversions = 0;
   if (right <= left)
     return number_of_inversions;
 
   size_t ave = left + (right - left) / 2;
-  number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave + 1, right);
+  number_of_inversions += get_number_of_inversions(a, left, ave);
+  number_of_inversions += get_number_of_inversions(a, ave + 1, right);
 
-  vector<int> left_vec(ave - left + 1);
-  std::move(a.begin() + left, a.begin() + ave + 1, left_vec.begin());
-  vector<int> right_vec(right - ave);
-  std::move(a.begin() + ave + 1, a.begin() + right + 1, right_vec.begin());
+  vector<int> left_vec(std::make_move_iterator(a.begin() + left), std::make_move_iterator(a.begin() + ave + 1));
+  vector<int> right_vec(std::make_move_iterator(a.begin() + ave + 1), std::make_move_iterator(a.begin() + right + 1));
   number_of_inversions += merge(a, left_vec, right_vec, right);
 
   return number_of_inversions;
@@ -68,6 +68,6 @@ int main()
   {
     std::cin >> a[i];
   }
-  vector<int> b(a.size());
-  std::cout << get_number_of_inversions(a, b, 0, a.size() - 1) << '\n';
+  long long inversions = get_number_of_inversions(a, 0, a.size() - 1);
+  std::cout << inversions << '\n';
 }
