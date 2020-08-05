988d21f9604bf9b7f9cd11f6f08b2404bb0ca53e
vijendra1125 <vijendrasingh003@gmail.com>
2020-08-05 23:22:27 +0530

    week3 pa 2

--- data_structures/programming_assignments/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.cpp
+++ data_structures/programming_assignments/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.cpp
@@ -2,41 +2,50 @@
 #include <vector>
 #include <algorithm>
 
-using std::vector;
 using std::cin;
 using std::cout;
+using std::make_pair;
+using std::pair;
+using std::vector;
 
-class JobQueue {
- private:
+class JobQueue
+{
+private:
   int num_workers_;
   vector<int> jobs_;
 
   vector<int> assigned_workers_;
   vector<long long> start_times_;
 
-  void WriteResponse() const {
-    for (int i = 0; i < jobs_.size(); ++i) {
+  void WriteResponse() const
+  {
+    for (int i = 0; i < jobs_.size(); ++i)
+    {
       cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
     }
   }
 
-  void ReadData() {
+  void ReadData()
+  {
     int m;
     cin >> num_workers_ >> m;
     jobs_.resize(m);
-    for(int i = 0; i < m; ++i)
+    for (int i = 0; i < m; ++i)
       cin >> jobs_[i];
   }
 
-  void AssignJobs() {
+  void AssignJobsNaive()
+  {
     // TODO: replace this code with a faster algorithm.
     assigned_workers_.resize(jobs_.size());
     start_times_.resize(jobs_.size());
     vector<long long> next_free_time(num_workers_, 0);
-    for (int i = 0; i < jobs_.size(); ++i) {
+    for (int i = 0; i < jobs_.size(); ++i)
+    {
       int duration = jobs_[i];
       int next_worker = 0;
-      for (int j = 0; j < num_workers_; ++j) {
+      for (int j = 0; j < num_workers_; ++j)
+      {
         if (next_free_time[j] < next_free_time[next_worker])
           next_worker = j;
       }
@@ -45,16 +54,78 @@ class JobQueue {
       next_free_time[next_worker] += duration;
     }
   }
+  void AssignJobsFast()
+  {
+    assigned_workers_.resize(jobs_.size());
+    start_times_.resize(jobs_.size());
+    pair<int, long long> thread_finish_time[num_workers_];
+    for (int i = 0; i < num_workers_; ++i)
+    {
+      assigned_workers_[i] = i;
+      start_times_[i] = 0;
+      thread_finish_time[i] = make_pair(i, jobs_[i]);
+    }
 
- public:
-  void Solve() {
+    int priority_job_id = num_workers_;
+    while (priority_job_id < jobs_.size())
+    {
+      int parent_id = 0;
+      while (((2 * parent_id) + 1) < num_workers_)
+      {
+        int child_id = (2 * parent_id) + 1;
+        if (((2 * parent_id) + 2) < num_workers_)
+        {
+          if (thread_finish_time[child_id].second > thread_finish_time[(2 * parent_id) + 2].second)
+          {
+            child_id = (2 * parent_id) + 2;
+          }
+        }
+        if (thread_finish_time[parent_id].second > thread_finish_time[child_id].second)
+        {
+          pair<int, long long> temp = thread_finish_time[parent_id];
+          thread_finish_time[parent_id] = thread_finish_time[child_id];
+          thread_finish_time[child_id] = temp;
+          parent_id = child_id;
+        }
+        else if (thread_finish_time[parent_id].second == thread_finish_time[child_id].second)
+        {
+          if (thread_finish_time[parent_id].first > thread_finish_time[child_id].first)
+          {
+            pair<int, long long> temp = thread_finish_time[parent_id];
+            thread_finish_time[parent_id] = thread_finish_time[child_id];
+            thread_finish_time[child_id] = temp;
+            parent_id = child_id;
+          }
+          else
+          {
+            break;
+          }
+        }
+        else
+        {
+          break;
+        }
+      }
+
+      start_times_[priority_job_id] = thread_finish_time[0].second;
+      assigned_workers_[priority_job_id] = thread_finish_time[0].first;
+      thread_finish_time[0].second += jobs_[priority_job_id];
+      priority_job_id++;
+    }
+  }
+
+public:
+  void Solve()
+  {
     ReadData();
-    AssignJobs();
+    // AssignJobsNaive();
+    AssignJobsFast();
     WriteResponse();
   }
 };
 
-int main() {
+int main()
+{
   std::ios_base::sync_with_stdio(false);
   JobQueue job_queue;
   job_queue.Solve();
